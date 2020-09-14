2e7803f743cef70cebfb74931c3c2abdf97bc03e
Alex Welsh <welsh6263@gmail.com>
2020-09-14 01:05:24 -0400

    start of Lab1

---
+++ Lab1/my_malloc.c
@@ -0,0 +1,25 @@
+//
+// Created by Alex Welsh on 9/14/20.
+//
+
+#include "my_malloc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+int main(int argc, char *argv[]){
+
+}
+
+void *my_malloc(size_t size){
+
+}
+
+void my_free(void *ptr){
+
+}
+
+FreeListNode free_list_begin(void){
+
+}
